aa20079b13f8a6c576a890ea46ffa50d995cd041
MohdJavedkhan93 <javedk.7491@gmail.com>
2022-04-14 18:08:37 +0530

    add test.js file

---
+++ test.js
@@ -0,0 +1,13 @@
+let num1 = 5;
+let num2 = 10;
+console.log("num1", num1)
+console.log("num2", num2)
+let temp = num1;
+num1 = num2;
+num2 = temp;
+console.log("num1", num1)
+console.log("num2", num2)
+
+for(let num=1; num<=10; num++){
+    console.log(3*num)
+}
