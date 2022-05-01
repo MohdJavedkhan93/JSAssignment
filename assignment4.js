a1cf747c418839b33208f8617ca67303f1bd6356
MohdJavedkhan93 <javedk.7491@gmail.com>
2022-05-01 06:43:10 +0530

    add assignment

---
+++ assignment4.js
@@ -0,0 +1,30 @@
+// Ques 1: we have an array of employees along with their years of work experience.
+// We only want the total years of all employees’ work experience as the output.
+
+var employees = [
+  { id: 111, name: "Chelsea Foster", years: 7 },
+  {
+    id: 102,
+    name: "Aggie Sparling",
+    years: 13,
+  },
+  {
+    id: 123,
+    name: "Timmy Matthews",
+    years: 23,
+  },
+  {
+    id: 119,
+    name: "Emmet Foster",
+    years: 22,
+  },
+];
+var totalyears = employees.map((workexp) => workexp.years);
+// console.log(totalyears);
+var totalyears = [7, 13, 23, 22];
+var sum = 0;
+var n = totalyears.length;
+for (let i = 0; i < n; i++) {
+  sum += totalyears[i];
+}
+console.log(sum);
---
+++ lecture.js
@@ -0,0 +1,11 @@
+var x = 10;
+
+if (x === 10) {
+//   var x = 20;
+
+  console.log(x);
+
+}
+
+console.log(x); 
+                           
