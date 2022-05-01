86aa3dbd3ea3e26c43aec06009e78e332b93a876
MohdJavedkhan93 <javedk.7491@gmail.com>
2022-05-01 08:02:34 +0530

    complete assignment.4js

--- assignment4.js
+++ assignment4.js
@@ -54,3 +54,38 @@ var members = [
 ];
 var allinterns = members.filter((interns) => interns.position == "Intern");
 console.log(allinterns);
+
+var members = [
+  {
+    id: 111,
+    name: "Chelsea Foster",
+    workExp: 7,
+    deptExp: 6,
+    isPermanent: true,
+  },
+  {
+    id: 102,
+    name: "Aggie Sparling",
+    workExp: 13,
+    deptExp: 10,
+    isPermanent: false,
+  },
+  {
+    id: 123,
+    name: "Timmy Matthews",
+    workExp: 23,
+    deptExp: 15,
+    isPermanent: false,
+  },
+  { id: 66, name: "Emmet Foster", workExp: 22, deptExp: 9, isPermanent: true },
+  { id: 89, name: "Brent Dolan", workExp: 16, deptExp: 12, isPermanent: true },
+];
+
+const permemployees = members.filter(
+  (permanent) => permanent.isPermanent === true
+);
+console.log(permemployees);
+const nonpermemp = members.filter(
+  (permanent) => permanent.isPermanent === false
+);
+console.log(nonpermemp);
